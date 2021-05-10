62bfcc8d4155c5e97430ad24398af6059510951b
SebastianRobak <67648371+SebastianRobak@users.noreply.github.com>
2021-05-10 12:03:43 +0200

    Add files via upload

--- Select_Category.py
+++ Select_Category.py
@@ -5,73 +5,77 @@ from os.path import join
 from tqdm import tqdm
 import json
 
+"""
+Class to use to filter coco categories
+"""
 
-class coco_category_filter:
-    def __init__(self, json_path, imgs_dir, categ='person'):
-        self.coco = COCO(json_path)
+
+class CocoFilter:
+    def __init__(self, json_path, images_path, categories='person'):
         self.json_path = json_path
-        self.imgs_dir = imgs_dir
-        self.categ = categ
-        self.images = self.get_imgs()
+        self.coco = COCO(json_path)
+        self.images_path = images_path
+        self.categories = categories
+        self.images = self.get_images()
+        self.category_ids = []
 
-    def get_imgs(self):
-        catIds = self.coco.getCatIds(catNms=[self.categ])
-        print("catIds: ", catIds)
-        imgIds = self.coco.getImgIds(catIds=catIds)
-        images = self.coco.loadImgs(imgIds)
-        self.catIds = catIds
+    # Function to find images with categories that were chosen
+    def get_images(self):
+        category_ids = self.coco.getCatIds(catNms=[self.categories])
+        print("category_ids: ", category_ids)
+        images_ids = self.coco.getImgIds(catIds=category_ids)
+        images = self.coco.loadImgs(images_ids)
+        self.category_ids = category_ids
         return images
 
-    def save_imgs(self):
-        os.makedirs(self.imgs_dir, exist_ok=True)
+    # Function to download coco dataset
+    def download_images(self):
+        os.makedirs(self.images_path, exist_ok=True)
         for im in tqdm(self.images):
             img_data = requests.get(im['coco_url']).content
-            with open(os.path.join(self.imgs_dir, im['file_name']), 'wb') as handler:
+            with open(os.path.join(self.images_path, im['file_name']), 'wb') as handler:
                 handler.write(img_data)
 
-    def filter_by_category(self, new_json_path):
-        json_parent = os.path.split(new_json_path)[0]
-        os.makedirs(json_parent, exist_ok=True)
-        imgs_ids = [x['id'] for x in self.images]  # get img_ids of imgs with the category
-        new_imgs = [x for x in self.coco.dataset['images'] if x['id'] in imgs_ids]
-        catIds = self.catIds
+    # Function that filters json file to only images with categories that were chosen
+    def filter_category(self, new_json_path):
+        json_oryginal = os.path.split(new_json_path)[0]
+        os.makedirs(json_oryginal, exist_ok=True)
+        images_ids = [i['id'] for i in self.images]
+        new_images = [i for i in self.coco.dataset['images'] if i['id'] in images_ids]
+        category_ids = self.category_ids
         # Filter annotations
-        new_annots = [x for x in self.coco.dataset['annotations'] if x['category_id'] in catIds]
+        new_annotations = [i for i in self.coco.dataset['annotations'] if i['category_id'] in category_ids]
         # Reorganize the ids
-        new_imgs, annotations = self.modify_ids(new_imgs, new_annots)
+        new_images, annotations = self.sequence_ids(new_images, new_annotations)
         # Filter categories
-        new_categories = [x for x in self.coco.dataset['categories'] if x['id'] in catIds]
+        new_categories = [i for i in self.coco.dataset['categories'] if i['id'] in category_ids]
         data = {
             "info": self.coco.dataset['info'],
             "licenses": self.coco.dataset['licenses'],
-            "images": new_imgs,
-            "annotations": new_annots,
+            "images": new_images,
+            "annotations": new_annotations,
             "categories": new_categories
         }
         with open(new_json_path, 'w') as f:
             json.dump(data, f)
 
-    def modify_ids(self, images, annotations):
-        old_new_imgs_ids = {}
-        for n, im in enumerate(images):
-            old_new_imgs_ids[images[n]['id']] = n + 1
-            images[n]['id'] = n + 1
-        for n, ann in enumerate(annotations):
-            annotations[n]['id'] = n + 1
-            old_image_id = annotations[n]['image_id']
-            annotations[n]['image_id'] = old_new_imgs_ids[old_image_id]
+    # Function that changes ids of images to be in sequence
+    def sequence_ids(self, images, annotations):
+        images_ids = {}
+        for i, image in enumerate(images):
+            images_ids[images[i]['id']] = i + 1
+            images[i]['id'] = i + 1
+        for i, annotation in enumerate(annotations):
+            annotations[i]['id'] = i + 1
+            old_image_id = annotations[i]['image_id']
+            annotations[i]['image_id'] = images_ids[old_image_id]
         return images, annotations
 
 
-def main(subset, year, root_dir, category='person'):
-    json_file = join(os.path.split(root_dir)[0], 'instances_' + subset + year + '.json')
-    imgs_dir = join(root_dir, category + '_' + subset)
-    new_json_file = join(root_dir, 'annotations', subset + ".json")
-    coco_filter = coco_category_filter(json_file, imgs_dir, categ=category)
-    coco_filter.save_imgs()
-    coco_filter.filter_json_by_category(new_json_file)
-
-    # Example usage:
-    # subset, year = 'train', '2017'  # val - train
-    # root_dir = '/home/ubuntu/coco_person_ds/coco_dataset'
-    # main(subset, year, root_dir, category='person')
+def main(dataset, year, path, categories='person'):
+    json_file = join(os.path.split(path)[0], 'instances_' + dataset + year + '.json_file')
+    images_path = join(path, categories + '_' + dataset)
+    new_json_file = join(path, 'annotations', dataset + ".json_file")
+    filter_category = CocoFilter(json_file, images_path, categ=categories)
+    filter_category.download_images()
+    filter_category.filter_category(new_json_file)
